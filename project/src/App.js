758858d67e1fee7e046e89ada0aac073eed7cf94
Yurimurda <lolsgotpwnd@yahoo.com>
2020-01-06 15:34:45 -0600

    test commit

--- project/src/App.js
+++ project/src/App.js
@@ -4,7 +4,7 @@ import logo from './logo.svg';
 import './App.css';
 
 // Component section start
-// import myContext from './context/myContext';
+import myContext from './context/myContext';
 
 // Component section end
 
