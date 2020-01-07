748cb2f2ddfa969c5c1c9a4b418ff2879c8c22be
Devanee Reid <devaneereid@gmail.com>
2020-01-06 17:34:59 -0700

    added react router dom

--- project/src/utils/axiosWithAuth.js
+++ project/src/utils/axiosWithAuth.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
-export const axiosWithAuth = () => {
+const axiosWithAuth = () => {
     const token = localStorage.getItem('token');
         return axios.create({
-            baseURL: 'https://chef-portfolio-be.herokuapp.com/login',
+            baseURL: 'https://chef-portfolio-be.herokuapp.com/api',
             headers: {
                 Authorization: token
             }
         });
     };
+
+export default axiosWithAuth;
