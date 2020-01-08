8f8d72bc14f451f56f10bf5f7eede9603e76d806
Yurimurda <lolsgotpwnd@yahoo.com>
2020-01-08 10:40:17 -0600

    test

---
+++ project/src/components/home/recipeList.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import SearchForm from "./searchForm";
+export default function RecipeList() {
+
+  const [data, setData] = useState([]);
+  useEffect(() => {
+
+    axios
+      .get(`https://chef-portfolio-be.herokuapp.com/recipes/`)
+        .then(response =>{
+          
+          console.log(response);
+          setData(response.data.results);
+        })
+        .catch(error =>{
+          console.log(error);
+        });
+      }, []);
+  return( 
+    <div><SearchForm data={data}/></div> 
+  )
+}
