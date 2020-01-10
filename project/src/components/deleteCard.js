5a002bafe9c988d8eccdd61c5df3af5946a66118
Yurimurda <lolsgotpwnd@yahoo.com>
2020-01-09 23:45:15 -0600

    recipe_id returning undefined

--- project/src/components/deleteCard.js
+++ project/src/components/deleteCard.js
@@ -1,21 +1,37 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import axiosWithAuth from '../utils/axiosWithAuth';
+import { useParams } from 'react-router-dom';
 
-export default function DeleteCard()  {
-    console.log(DeleteCard);
-    // axiosWithAuth()
-    //     .delete(`https://chef-portfolio-be.herokuapp.com/chef/${id.chef_id}/recipes/${id.recipe_id}`)
-    //     .then(res => {
-    //         console.log(res);
-    //         updateColors(colors.filter(el => el.id !== color.id));
-    //     })
-    //     .catch(err => console.log(err));
+export default function DeleteCard() {
+    // const [deletedRecipe, setDeletedRecipe] = useState([])
+const { recipe_id } = useParams();
 
-        return (
-            <button type="submit" onClick={() => DeleteCard()}>
-                Delete Recipe
-              </button>
-        );
+
+
+const onSubmit = e =>{
+    const id = recipe_id;
+    // e.preventDefault();
+    axiosWithAuth()
+    .delete(`https://chef-portfolio-be.herokuapp.com/chef/1/recipes/${id}`)
+    .then(response => {
+        console.log("Successful delete:", response);
+        console.log(response.data.recipe_id);
+    })
+    .catch(error => console.log(error.response));
+    
 };
 
+return(
+    <button type="submit" onClick={onSubmit}>
+        
+        Delete Recipe
+      </button>
+);
+
+
+};
+
+
+
+// export default DeleteCard;
